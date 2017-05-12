dcac9e17a604936eeb90743a5ea5a4c8e92e8819
Sebastian Morr <sebastian@morr.cc>
2017-05-12 16:34:32 +0200

    Generate Anki plugin's JSON format

--- generate-notes.rb
+++ generate-notes.rb
@@ -3,31 +3,38 @@ require "json"
 require "base91"
 require "digest"
 
-fonts = YAML.load(File.read("fonts.yaml"))
+#fonts = YAML.load(File.read("fonts.yaml"))
+#
+#notes = []
+#
+#fonts.each_pair do |name, attributes|
+#    attributes = {} if attributes.nil?
+#
+#    entry = {
+#        "__type__" => "Note",
+#        "data" => "",
+#        "fields" => [
+#            name,
+#            "<img src=\"#{name.gsub(" ", "_")}.svg\" />",
+#            attributes["characteristics"].to_s,
+#            attributes["history"].to_s
+#        ],
+#        "flags" => 0,
+#        "guid" => Base91.encode(Digest::SHA256.digest(name))[0..8],
+#        "note_model_uuid" => "17cca788-3703-11e7-8da1-448500519c3a",
+#        "tags" => []
+#    }
+#
+#    notes << entry
+#end
 
-notes = []
+hash = JSON.load(File.read("Fonts/Fonts.json"))
+json = JSON.pretty_generate(hash)
 
-fonts.each_pair do |name, attributes|
-    attributes = {} if attributes.nil?
+json.gsub!("  ", "    ")
+json.gsub!(/,$/, ", ")
+json.gsub!(/\[\s+\]/, "[]")
 
-    entry = {
-        "__type__" => "Note",
-        "data" => "",
-        "fields" => [
-            name,
-            "<img src=\"#{name.gsub(" ", "_")}.svg\" />",
-            attributes["characteristics"].to_s,
-            attributes["history"].to_s
-        ],
-        "flags" => 0,
-        "guid" => Base91.encode(Digest::SHA256.digest(name))[0..8],
-        "note_model_uuid" => "17cca788-3703-11e7-8da1-448500519c3a",
-        "tags" => []
-    }
+#h["notes"] = notes
 
-    notes << entry
-end
-
-h = JSON.load(File.read("json-template.json"))
-h["notes"] = notes
-IO.write("Fonts/Fonts.json", JSON.pretty_generate(h))
+IO.write("Fonts/Fonts.json", json)
