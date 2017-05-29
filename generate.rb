9c39408d6c5aeb7f8e2c9c2cce1d9b0e27cf2a56
Sebastian Morr <sebastian@morr.cc>
2017-05-29 20:19:46 +0200

    Script to regenerate JSON

--- generate.rb
+++ generate.rb
@@ -5,18 +5,17 @@ def generate(name)
 
     results = `fc-list | grep '#{font}'`
     if results == ""
-        STDERR.puts "not found on system. Aborting."
-        return false
+        puts "not found on system."
+    else
+
+        svg = IO.read("specimen-template.svg")
+        svg.gsub!("Arial", font)#.gsub!("normal", weight)
+        IO.write("/tmp/specimen.svg", svg)
+
+        `inkscape --export-text-to-path --export-plain-svg /tmp/specimen-plain.svg /tmp/specimen.svg`
+        `svgo /tmp/specimen-plain.svg`
+        `cp /tmp/specimen-plain.svg Fonts/media/#{name.gsub(" ", "_")}.svg`
+
+        puts "done."
     end
-
-    svg = IO.read("specimen-template.svg")
-    svg.gsub!("Arial", font)#.gsub!("normal", weight)
-    IO.write("/tmp/specimen.svg", svg)
-
-    `inkscape --export-text-to-path --export-plain-svg /tmp/specimen-plain.svg /tmp/specimen.svg`
-    `svgo /tmp/specimen-plain.svg`
-    `cp /tmp/specimen-plain.svg Fonts/media/#{name.gsub(" ", "_")}.svg`
-
-    puts "done."
-    return true
 end
---
+++ regenerate-json.rb
@@ -0,0 +1,21 @@
+require "yaml"
+require "json"
+require "./generate"
+
+images = []
+
+hash = JSON.load(File.read("Fonts/Fonts.json"))
+hash["notes"].each do |note|
+    image = note["fields"][1][10..-5]
+    images << image
+end
+
+hash["media_files"] = images
+
+json = JSON.pretty_generate(hash)
+json.gsub!("  ", "    ")
+json.gsub!(/,$/, ", ")
+json.gsub!(/\[\s+\]/, "[]")
+
+puts json
+IO.write("Fonts/Fonts.json", json)
